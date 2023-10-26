4d7274300d053f900e09db542c746c1bb7241ace
Kasthuri <tkasthu.90@gmail.com>
2023-10-26 11:15:04 +1100

    Initial commit

---
+++ GitExample/GitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExampleApp.swift
+//  GitExample
+//
+//  Created by Kasthuri Tandavarayane on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
